009191b22c9633aa541e42e46f828fb4461b0608
givemecookies-create <85323904+givemecookies-create@users.noreply.github.com>
2023-04-06 20:35:06 -0700

    Update uv.config.js
    switched it again

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://vampeds-unblocker.onrender.com/bare/',
+    bare: 'vamped-heads-interstellar-hoster.vercel.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
